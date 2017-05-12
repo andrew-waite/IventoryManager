40be40233d02ecce577228449e21643bebd8843f
Andrew Waite <drewrockz01@gmail.com>
2017-05-12 18:57:32 +1000

    Initial Commit

---
+++ src/me/eaglehawk/Main.java
@@ -0,0 +1,6 @@
+package me.eaglehawk;
+
+public class Main
+{
+
+}
---
+++ test/me/eaglehawk/MainTest.java
@@ -0,0 +1,16 @@
+package me.eaglehawk;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainTest 
+{
+
+	@Test
+	public void test() 
+	{
+		fail("Not yet implemented");
+	}
+
+}
